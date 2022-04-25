4ad32382d63260ee5f382d6b99c69ac6bf5f728a
Max Sherwin <masherwin@wisc.edu>
2022-04-25 15:07:21 -0500

    added simulation script

--- scripts/simulation.jl
+++ scripts/simulation.jl
@@ -1,12 +1,12 @@
-for simulation_rep 1:1
+for simulation_rep in 1:2
 
   #run simphy and generate files
-  simphy_command = './SimPhy -i configuration_files/simulation -o outfiles/sim_out'
-  run(simphy_command)
+  string_rep = string(simulation_rep)
+  run(`../Sim-Phy-Source/SimPhy -i ../simphy-configs/simphysim-conf -o ../outfiles/sim_out$string_rep`)
 
   for gene in 1:1 #start with 1 get to 1000
+    
 
-    ## PLAN ON USING THE L_TREES FILE FOR THE SIMULATION SO I ONLY HAVE TO READ ONE FILE
     #run seq-gen
 
     #run IQ alignment for later
@@ -14,3 +14,4 @@ for simulation_rep 1:1
   #run astral
   #run snaq
   #run PhylonetMPL
+end
