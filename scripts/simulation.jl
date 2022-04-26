f4b5aa6d4c48e99f47a691f779c4a1c4140e89f0
Max Sherwin <masherwin@wisc.edu>
2022-04-25 22:59:28 -0500

    updated git commit file

--- scripts/simulation.jl
+++ scripts/simulation.jl
@@ -4,15 +4,20 @@ run(`mkdir ../seq-gen-outfiles`)
 
 run(`../Sim-Phy-Source/SimPhy -i ../simphy-configs/simphysim-conf -o ../sim-phy-outfiles/sim_out`)
 
-for simulation_rep in 1:2
+for simulation_rep in 1:2 #1000 on final
   repition_string = string(simulation_rep)
   run(`mkdir ../seq-gen-outfiles/simphy$repition_string`)
-  for gene_tree in 1:10
+  for gene_tree in 1:10 # 1000 on final
     
     tree_string = string(gene_tree)
+    #account for sim phy naming convention
     if length(tree_string) == 1
-      tree_string = "0" * tree_string
+      tree_string = "000" * tree_string
     elseif length(tree_string) == 2
+      tree_string = "00" * tree_string
+    elseif length(tree_string) == 3
+      tree_string = "0" * tree_string
+    elseif length(tree_string) == 4
       tree_string = tree_string
     end
 
