ce51e0fb78da4258a86b025809acc04c6aaaf6ee
Max Sherwin <masherwin@wisc.edu>
2022-05-12 10:48:05 -0500

    added gene reps vs simulation reps

--- scripts/simulation.jl
+++ scripts/simulation.jl
@@ -12,13 +12,13 @@ run(`../executables/SimPhy -i ../simphy-configs/simphysim-conf-new -o ../sim-phy
 
 for simulation_rep in 1:n_reps #1000 on final number of replicates is hardcoded in config file
   repition_string = string(simulation_rep)
-  repition_string = lapd(repition_string, ceil(Int, log10(n+1)), '0')
+  repition_string = lapd(repition_string, ceil(Int, log10(n_reps+1)), '0')
   run(`mkdir ../seq-gen-outfiles/simphy$repition_string`)
   for gene_tree in 1:n_genes # 1000 on final
     
     tree_string = string(gene_tree)
     #account for sim phy naming convention
-    tree_string = lpad(tree_string, ceil(Int, log10(n+1)), '0')
+    tree_string = lpad(tree_string, ceil(Int, log10(n_genes+1)), '0')
 
     #runs seq gen for each tree in this rep of the simulation
     #change to a pipeline
@@ -27,11 +27,11 @@ for simulation_rep in 1:n_reps #1000 on final number of replicates is hardcoded 
   end
 end
 for simulation_rep in 1:n_reps
-  simulation_rep = lpad(simulation_rep, ceil(Int, log10(n+1)), '0')
+  simulation_rep = lpad(simulation_rep, ceil(Int, log10(n_reps+1)), '0')
   run(`perl raxml.pl --seqdir=../seq-gen-outfiles/simphy$simulation_rep --raxmldir=raxml-outfiles$simulation_rep --astraldir=astral-outfiles$simulation_rep`)
 end
 for simulation_rep in 1:n_reps
-  simulation_rep = lpad(simulation_rep, ceil(Int, log10(n+1)), '0')
+  simulation_rep = lpad(simulation_rep, ceil(Int, log10(n_reps+1)), '0')
   run(`mv raxml-outfiles$simulation_rep ../raxml-outfiles`)
   run(`mv astral-outfiles$simulation_rep ../astral-outfiles`)
 end
