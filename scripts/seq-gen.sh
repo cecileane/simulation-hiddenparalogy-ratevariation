bdc8c7328ec9afd6262f57f4953874de42ae5d04
Cecile Ane <cecileane@users.noreply.github.com>
2024-10-03 15:40:13 -0500

    main simulation script: changed folder structure, more to do

--- scripts/seq-gen.sh
+++ scripts/seq-gen.sh
@@ -1,1 +1,1 @@
-../executables/seq-gen  -l1000 -mHKY -a0.356 -t4.143 -f0.316,0.182,0.183,0.319 -on < ../sim-phy-outfiles/sim_out/$1/g_trees$2.trees > ../seq-gen-outfiles/simphy$1/tree$2.dat
+executables/seq-gen  -l1000 -mHKY -a0.356 -t4.143 -f0.316,0.182,0.183,0.319 -on < $3/sim-phy-outfiles/$1/g_trees$2.trees > $3/seq-gen-outfiles/rep$1/gene$2.nex
--- scripts/simulation.jl
+++ scripts/simulation.jl
@@ -1,38 +1,71 @@
-n_reps=2
-n_genes=10
-#on darwin cluster use the /nobackup dir with tmux
-mkdir("../sim-phy-outfiles")
-mkdir("../seq-gen-outfiles")
-mkdir("../raxml-outfiles")
-mkdir("../astral-outfiles")
+#=
+this script does:
 
+1.
+2.
+3.
 
-#do I need a for loop here or is the simphy config sufficent?
-run(`../executables/SimPhy -i ../simphy-configs/simphysim-conf-new -o ../sim-phy-outfiles/sim_out`)
+example: run this below
+ on darwin cluster use the `/nobackup` dir with tmux
+ on franlin use `/nobackup2`
+ from the root of the repository:
 
-for simulation_rep in 1:n_reps #1000 on final number of replicates is hardcoded in config file
-  repition_string = string(simulation_rep)
-  repition_string = lpad(repition_string, ceil(Int, log10(n_reps+1)), '0')
-  run(`mkdir ../seq-gen-outfiles/simphy$repition_string`)
-  for gene_tree in 1:n_genes # 1000 on final
-    
-    tree_string = string(gene_tree)
-    #account for sim phy naming convention
-    tree_string = lpad(tree_string, ceil(Int, log10(n_genes+1)), '0')
+julia scripts/simulation.jl
 
-    #runs seq gen for each tree in this rep of the simulation
-    #change to a pipeline
-    run(`bash seq-gen.sh $repition_string $tree_string`)
-  
+output: it will create folder xxx with inside:
+- 
+- 
+=#
+
+# set all configuration parameters here
+duploss = "no"
+ratevar = "yes"
+n_reps = 2
+n_genes = 10
+# number of sites / gene is always 1000: hard-coded into seq-gen.sh
+
+rootfolder = pwd()
+outfolder = "output/DL$duploss-RV$ratevar"
+mkpath(outfolder)
+simphyfolder = joinpath(outfolder, "sim-phy-outfiles")
+seqgenfolder = joinpath(outfolder, "seq-gen-outfiles")
+raxmlfolder  = joinpath(outfolder, "raxml-outfiles")
+astralfolder = joinpath(outfolder, "astral-outfiles")
+mkdir(simphyfolder)
+mkdir(seqgenfolder)
+mkdir(raxmlfolder)
+mkdir(astralfolder)
+
+# todo: add lines of code to create specific configuation file:
+# - modify the "new" simphysim-conf configure file to have less, named "master"
+# - here: read the master
+# - add stuff to the string to add n_reps, n_genes & seed
+# - write specific configuration file in simphyfolder
+
+run(`executables/SimPhy -i $(simphyfolder)/simphysim-conf-new -o $(simphyfolder)/sim-phy-outfiles`)
+
+# run seq-gen on each replicate and each gene
+for simulation_rep in 1:n_reps
+  rep_number_string = lpad(string(simulation_rep), ceil(Int, log10(n_reps+1)), '0')
+  run(`mkdir ../seq-gen-outfiles/simphy$rep_number_string`)
+  for gene_tree in 1:n_genes
+    # account for sim phy naming convention
+    genenum_string = lpad(string(gene_tree), ceil(Int, log10(n_genes+1)), '0')
+    # seq-gen.sh knows about the directory structure & names
+    run(`bash executable/seq-gen.sh $rep_number_string $tree_string $outfolder`)
   end
 end
+
+# run raxml on each gene of each rep: use raxml.pl on each rep
 for simulation_rep in 1:n_reps
   simulation_rep = lpad(simulation_rep, ceil(Int, log10(n_reps+1)), '0')
-  run(`perl raxml.pl --seqdir=../seq-gen-outfiles/simphy$simulation_rep --raxmldir=raxml-outfiles$simulation_rep --astraldir=astral-outfiles$simulation_rep`)
+  run(`perl raxml.pl --seqdir=$seqgenfolder/simphy$simulation_rep --raxmldir=$raxmlfolder/raxml-outfiles$simulation_rep --astraldir=$astralfolder/astral-outfiles$simulation_rep`)
 end
+#= remove this below if all works well
 for simulation_rep in 1:n_reps
   simulation_rep = lpad(simulation_rep, ceil(Int, log10(n_reps+1)), '0')
-  run(`mv raxml-outfiles$simulation_rep ../raxml-outfiles`)
-  run(`mv astral-outfiles$simulation_rep ../astral-outfiles`)
+  run(`mv raxml-outfiles$simulation_rep $raxmlfolder`)
+  run(`mv astral-outfiles$simulation_rep $astralfolder`)
 end
+=#
 
