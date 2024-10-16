9268d98fda7fe0c2c66e81884af7f91c78a15498
bingli8899 <bli283@wisc.edu>
2024-10-16 17:18:18 -0500

    add argParse

--- scripts/simulation.jl
+++ scripts/simulation.jl
@@ -8,6 +8,7 @@ this script does:
 example: run this below
  on darwin cluster use the `/nobackup` dir with tmux
  on franlin use `/nobackup2`
+ on UW-Madison Botany pink server 
  from the root of the repository:
 julia scripts/simulation.jl
 
@@ -18,53 +19,72 @@ output: it will create folder 'output' in the root of the repo with inside:
 - astral-outfiles: Inferred species tree
 =#
 
+## Now, start to add in ArgParse package 
 
+using ArgParse 
 
+function parse_commandline()
+  s = ArgParseSettings() 
+  @add_arg_table s begin 
+    "--duploss"
+      help = "Gene duplication rate: specify the gene duplication rate, if 0, then no duplication"
+      arg_type = Number
+      required = true
+    "--ratevar" 
+      help = "'N': No rate variation';\n 'G: Gene specific rate variation';\n 'L': Lineage specific rate variation;\n 'GL' or 'G*L': genexlineage rate variation"
+      arg_type = String
+      choices =  ['N', 'G', 'L', 'G*L']
+      required = true
+    "--n_reps"
+      help = "Number of replicates" 
+      arg_type = Int
+      required = true
+    "--n_genes"
+      help = "Number of genes" 
+      arg_type = Int
+      required = true
+    "--seed_simphy"
+      help = "Seed for simphy"
+      arg_type = Int
+      required = true
+  end 
+  return parse_args(s)
+end
 
-# todo for week Oct 7: add lines of code to create specific configuation file:
-# - modify the "new" simphysim-conf configure file to have less, named "master": Done 
-# - here: read the master: Done
-# - add stuff to the string to add n_reps, n_genes & seed: Done 
-# - write specific configuration file in simphyfolder: Done 
+function main()
+  parsed_args = parse_commandline()
+  println("Parsed args:")
+  for (arg,val) in parsed_args
+      println("  $arg  =>  $val")
+  end
 
-#= Previously, the duploss and ratevar are fixed 
-duploss = "no"
-ratevar = "yes"
-n_reps = 2
-n_genes = 10 # number of sites / gene is always 1000: hard-coded into seq-gen.sh
-=# 
+  duploss = parsed_args["duploss"] # A number indicates rate of gene duplication rates (0 = no dup)
+  ratevar = parsed_args["ratevar"] # "N", "G", "L" or "GL" or "G*L" to include genexlineage rate variation 
+  n_reps = parsed_args["n_reps"] # number of replicates 
+  n_genes = parsed_args["n_genes"] # number of genes 
+  seed_simphy = parsed_args["seed_simphy"] # seeds for SimPhy 
 
-#= Notes: 
-1) The very last part, which mv the output of raxml and astral to a folder, cannot be changed 
-the issue is called by line 75 in raxml.pl: die ("raxmldir should be only 1 level up\n") if ($raxmldir =~ /\//); If comment out this line, then error occurs when setting up the phylipdir 
-This could be resolved later or remain as it is 
-2) Parameters for rate variations 
-Based on README.md, it seems that only gene variation is examined. 
-How about lineage variation or gene x lienage variations? 
-3) I changed the last part of raxml.pl so that the final astral has one output -- need to double chec: Why do the original script generates boostrap trees? 
-This could be changed to a newer version of unweighted astral later 
-4) After changing the tips to letters, it seems that Seq-Gen
-=# 
+  println("duploss = $duploss, ratevar = $ratevar, n_reps = $n_reps, n_genes = $n_genes, seed = $seed_simphy")
+end
 
-duploss = ARGS[1] # Yes or No
-ratevar = ARGS[2] # "No", "Ge", "Li" or "GL” 
-# In meeting, rate var is "yes" or "no" 
-n_reps = parse(Int, ARGS[3])
-n_genes = parse(Int, ARGS[4]) 
-seed = parse(Int, ARGS[5])
-
-# Print out the arguments 
-println("Running simulation.pl for:\n")
-println("duploss = $duploss, ratevar = $ratevar, n_reps = $n_reps, n_genes = $n_genes, seed = $seed")
+main() 
 
 # set all configuration parameters here 
 rootfolder = pwd()
-outfolder = "output/DL$duploss-RV$ratevar"
+paramname_root = "DL$duploss-RV$ratevar"
+outfolder = "output/$paramname_root"
 mkpath(outfolder)
 simphyfolder = joinpath(outfolder, "sim-phy-outfiles")
 seqgenfolder = joinpath(outfolder, "seq-gen-outfiles")
-raxmlfolder  = joinpath(outfolder, "raxml-outfiles")
-astralfolder = joinpath(outfolder, "astral-outfiles")
+raxmlfolder_tmp_root = "raxml-outfiles"
+raxmlfolder  = joinpath(outfolder, raxmlfolder_tmp_root)
+astralfolder_tmp_root = "astral-outfiles"
+astralfolder = joinpath(outfolder, astralfolder_tmp_root)
+# temporary folders that need to be 1 level from the repo root folder, for raxml.pl,
+# later moved into their proper place down the folder hierarchy
+function tmp_raxmlastral_folders(params, rep)
+  return ("$raxmlfolder_tmp_root-$params-$rep", "$astralfolder_tmp_root-$params-$rep")
+end
 mkdir(simphyfolder)
 mkdir(seqgenfolder)
 mkdir(raxmlfolder)
@@ -75,31 +95,33 @@ conf_content = read(master_conf, String) # read the master config file into a st
 
 #Set up the parameters (# replications, # genes, and seeds)
 parameters = """
-# Parameters: DL:$duploss-RV:$ratevar, with replicate = $n_reps genes = $n_genes and seeds = $seed: 
+# Parameters: DL:$duploss-RV:$ratevar, with replicate = $n_reps genes = $n_genes and seeds = $seed_simphy: 
 -rs $n_reps  // Number of replicates
 -rl f:$n_genes  // Number of loci (genes) per replicate - f means a fixed value 
--cs $seed  // seed
+-cs $seed_simphy  // seed
 """
 
 # To simulate hidden paralogy, need to adjust duplication rate 
-if duploss == "Yes" # Here, not sure about the duplication rate, so choose a very arbitrary number 
+if duploss == "Y" # Here, not sure about the duplication rate, so choose a very arbitrary number 
   parameters *= "-lb f:0.0001 // Duplication rate\n" # This should be changed 
   # Think: should I use -lb loss rate as well? 
   # What would be the dup rate to use and loss rate to use? 
 end 
 
 # To simulate substitution rate variation
-if ratevar == "Ge" # gene-family-speciic rate heterogenity 
+if occursin("G", ratevar) # gene-family-speciic rate heterogenity : "G" or "GL"
   parameters *= "-hl ln:-0.19,0.6164414002968976 //log-normal distribution of gene rates"
-elseif ratevar == "Ln" # To simulate variation across lineages (ratevar = "Ln") 
-  parameters *= "-hs f:0.01" # An arbitrary number for now -- need to change 
-elseif ratevar == "GL"  # To simulate gene-by-lineage variation  
+end
+if occursin("L", ratevar) # To simulate variation across lineages (ratevar = "L" or "GL" or "G*L") 
+  parameters *= "-hs f:0.01" # An arbitrary number for now -- need to change
+end
+if occursin("G*L", ratevar)  # To simulate gene-by-lineage variation  
   parameters *= "-hh f:0.01" # An arbitrary number for now -- need to change 
 end # else ratevar == "No" -- no additional para to be specified 
 
-
+# change new_conf_file 
 combined_content = parameters * conf_content # combine parameters with master config
-new_conf_file = joinpath(simphyfolder, "simphysim-conf-DL$duploss-RV$ratevar")
+new_conf_file = joinpath(simphyfolder, "simphysim-conf-$paramname_root")
 write(new_conf_file, combined_content) # write the combined config into SimPhy output folder
 
 #-----------------------------------------------#       
@@ -129,17 +151,17 @@ end
 # run raxml on each gene of each rep: use raxml.pl on each rep
 for simulation_rep in 1:n_reps
   simulation_rep = lpad(simulation_rep, ceil(Int, log10(n_reps+1)), '0')
-  run(`perl ./scripts/raxml.pl --seqdir=$seqgenfolder/rep$simulation_rep --raxmldir=raxml-outfiles$simulation_rep --astraldir=astral-outfiles$simulation_rep`)
+  tmpraxmldir, tmpastraldir =  tmp_raxmlastral_folders(paramname_root, simulation_rep)
+  run(`perl ./scripts/raxml.pl --seqdir=$seqgenfolder/rep$simulation_rep --raxmldir=$tmpraxmldir --astraldir=$tmpastraldir`)
 end
 
-#removing this could cause issues with setting up raxmlfolder 
-# The issue came from the line in raxml.pl: die ("raxmldir should be only 1 level up\n") if ($raxmldir =~ /\//); 
-# Commenting out this line will cause further errors when setting up philp file directory 
-# This needs to be solved 
+# raxml.pl requires folders that are 1 level from where the script is run.
+# below: move these folders to their proper place
 for simulation_rep in 1:n_reps
   simulation_rep = lpad(simulation_rep, ceil(Int, log10(n_reps+1)), '0')
-  run(`mv raxml-outfiles$simulation_rep $raxmlfolder`)
-  run(`mv astral-outfiles$simulation_rep $astralfolder`)
+  tmpraxmldir, tmpastraldir =  tmp_raxmlastral_folders(paramname_root, simulation_rep)
+  run(`mv $tmpraxmldir  $raxmlfolder`)
+  run(`mv $tmpastraldir $astralfolder`)
 end
 
 
